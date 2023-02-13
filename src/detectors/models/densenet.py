985b361f82dc7db513ae241cf5b0de8d15957c51
Eduardo Dadalto <edadaltocg@gmail.com>
2023-02-13 11:50:32 +0100

    improved `models` api

--- src/detectors/models/densenet.py
+++ src/detectors/models/densenet.py
@@ -1,23 +1,73 @@
+import timm
+import timm.models
 import torch
 import torch.nn as nn
-import torchvision
+from detectors.data import CIFAR10_DEFAULT_MEAN, CIFAR10_DEFAULT_STD
+from detectors.data.constants import CIFAR100_DEFAULT_MEAN, CIFAR100_DEFAULT_STD
+from detectors.models.utils import ModelDefaultConfig
+from timm.models import register_model as timm_register_model
 
 
-def DenseNet121Small(num_classes=10):
-    model = torchvision.models.densenet._densenet(
-        growth_rate=12, block_config=(6, 12, 24, 16), num_init_features=24, weights=None, progress=False
+def _cfg(url="", **kwargs):
+    return ModelDefaultConfig(
+        url=url,
+        num_classes=10,
+        input_size=(3, 32, 32),
+        pool_size=(4, 4),
+        crop_pct=0.875,
+        interpolation="bilinear",
+        mean=CIFAR10_DEFAULT_MEAN,
+        std=CIFAR10_DEFAULT_STD,
+        first_conv="features.conv0",
+        classifier="classifier",
+        **kwargs,
     )
+
+
+default_cfgs = {
+    "densenet121_cifar10": _cfg(url="", architecture="densenet121"),
+    "densenet121_cifar100": _cfg(
+        url="", num_classes=100, mean=CIFAR100_DEFAULT_MEAN, std=CIFAR100_DEFAULT_STD, architecture="densenet121"
+    ),
+}
+
+
+def _create_densenet_small(variant, pretrained=False, **kwargs):
+    default_cfg = default_cfgs[variant]
+
+    # load timm model
+    architecture = default_cfg.architecture or variant.split("_")[0]
+    model = timm.create_model(architecture, pretrained=False)
+
+    # override timm config
+    model.default_cfg = default_cfg
+
+    # override model
     model.features.conv0 = nn.Conv2d(3, 24, kernel_size=3, padding=1, bias=False)
     del model.features.norm0
-    del model.features.relu0
     del model.features.pool0
+    model.classifier = nn.Linear(model.classifier.in_features, model.default_cfg.num_classes)
 
-    model.classifier = nn.Linear(model.classifier.in_features, num_classes)
+    # load weights
+    if pretrained:
+        checkpoint = model.default_cfg.url
+        model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=True, map_location="cpu"))
+
     return model
 
 
+@timm_register_model
+def densenet121_cifar10(pretrained=False, **kwargs):
+    return _create_densenet_small("densenet121_cifar10", pretrained=pretrained, **kwargs)
+
+
+@timm_register_model
+def densenet121_cifar100(pretrained=False, **kwargs):
+    return _create_densenet_small("densenet121_cifar100", pretrained=pretrained, **kwargs)
+
+
 if __name__ == "__main__":
-    model = DenseNet121Small()
-    print(model)
-    x = torch.randn(1, 3, 32, 32)
-    y = model(x)
+    model_obj_1 = timm.create_model("densenet121", pretrained=False)
+    model_obj_2 = timm.create_model("densenet121_cifar10", pretrained=False)
+    print(model_obj_1.features.conv0)
+    print(model_obj_2.features.conv0)
--- src/detectors/models/resnet.py
+++ src/detectors/models/resnet.py
@@ -1,102 +1,117 @@
-from typing import List, Optional, Union
-
+import timm
+import timm.models
 import torch
 import torch.nn as nn
-import torchvision
+from detectors.data import CIFAR10_DEFAULT_MEAN, CIFAR10_DEFAULT_STD
+from detectors.data.constants import CIFAR100_DEFAULT_MEAN, CIFAR100_DEFAULT_STD
+from detectors.models.utils import ModelDefaultConfig
+from timm.models import register_model as timm_register_model
 
-from . import register_model
+
+def _cfg(url="", **kwargs):
+    num_classes = kwargs.pop("num_classes", 10)
+    mean = kwargs.pop("mean", CIFAR10_DEFAULT_MEAN)
+    std = kwargs.pop("std", CIFAR10_DEFAULT_STD)
+    return ModelDefaultConfig(
+        url=url,
+        num_classes=num_classes,
+        input_size=(3, 32, 32),
+        pool_size=(4, 4),
+        crop_pct=1,
+        interpolation="bilinear",
+        mean=mean,
+        std=std,
+        first_conv="conv1",
+        classifier="fc",
+        **kwargs,
+    )
 
 
-def ResNet18Small(num_classes=10):
-    model = torchvision.models.resnet18(weights=None)
-    print(model.inplanes)
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+default_cfgs = {
+    "resnet18_cifar10": _cfg(url="", architecture="resnet18"),
+    "resnet34_cifar10": _cfg(url="", architecture="resnet34"),
+    "resnet50_cifar10": _cfg(url="", architecture="resnet50"),
+    "resnet18_cifar100": _cfg(
+        url="", num_classes=100, mean=CIFAR100_DEFAULT_MEAN, std=CIFAR100_DEFAULT_STD, architecture="resnet18"
+    ),
+    "resnet34_cifar100": _cfg(
+        url="", num_classes=100, mean=CIFAR100_DEFAULT_MEAN, std=CIFAR100_DEFAULT_STD, architecture="resnet34"
+    ),
+    "resnet50_cifar100": _cfg(
+        url="", num_classes=100, mean=CIFAR100_DEFAULT_MEAN, std=CIFAR100_DEFAULT_STD, architecture="resnet50"
+    ),
+}
+
+
+def _create_resnet_small(variant, features_dim=512, pretrained=False, **kwargs):
+    default_cfg = default_cfgs[variant]
+
+    # load timm model
+    architecture = default_cfg.architecture or variant.split("_")[0]
+    model = timm.create_model(architecture, pretrained=False)
+
+    # override timm config
+    model.default_cfg = default_cfg
+
+    # override model
+    model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
     model.maxpool = nn.Identity()  # type: ignore
-    model.avgpool = nn.AvgPool2d(4)  # type: ignore
-    model.fc = nn.Linear(512, num_classes)
-    return model
+    model.fc = nn.Linear(features_dim, model.default_cfg.num_classes)
 
-
-@register_model("resnet18_cifar10")
-def resnet18_cifar10(weights=False, **kwargs):
-    """
-    Resnet18 model with a small `conv1` layer and a linear layer with `num_classes` outputs
-
-    Args:
-        pretrained (bool): kwargs, load pretrained weights into the model
-    """
-    # Call the model, load pretrained weights
-    num_classes = kwargs.get("num_classes", 10)
-    model = ResNet18Small(num_classes=num_classes)
-
-    if weights:
-        checkpoint = "https://github.com/edadaltocg/detectors/releases/download/weights-v0.1.0-beta/best.pth"
+    # load weights
+    if pretrained:
+        checkpoint = model.default_cfg.url
         model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=True, map_location="cpu"))
 
     return model
 
 
-def ResNet34Small(num_classes=10):
-    model = torchvision.models.resnet34(weights=None)
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-    model.maxpool = nn.Identity()  # type: ignore
-    model.avgpool = nn.AvgPool2d(4)  # type: ignore
-    model.fc = nn.Linear(512, num_classes)
-    return model
+@timm_register_model
+def resnet18_cifar10(pretrained=False, **kwargs):
+    return _create_resnet_small("resnet18_cifar10", features_dim=512, pretrained=pretrained, **kwargs)
 
 
-@register_model("resnet34_cifar10")
-def resnet34_cifar10(weights: Optional[Union[str, bool]] = False, **kwargs):
-    """
-    Resnet34 model with a small `conv1` layer and a linear layer with 10 classes outputs
-
-    Args:
-        pretrained (bool): kwargs, load pretrained weights into the model
-    """
-    # Call the model, load pretrained weights
-    num_classes = kwargs.get("num_classes", 10)
-    model = ResNet34Small(num_classes=num_classes)
-
-    if weights:
-        if isinstance(weights, str):
-            checkpoint = weights
-        else:
-            checkpoint = (
-                "https://github.com/edadaltocg/detectors/releases/download/weights-v0.1.0-beta/resnet34_cifar10_42.pth"
-            )
-
-        model.load_state_dict(torch.hub.load_state_dict_from_url(checkpoint, progress=True, map_location="cpu"))
-
-    return model
+@timm_register_model
+def resnet34_cifar10(pretrained=False, **kwargs):
+    return _create_resnet_small("resnet34_cifar10", features_dim=512, pretrained=pretrained, **kwargs)
 
 
-def ResNet101Small(num_classes=10):
-    model = torchvision.models.resnet101(weights=None)
-    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-    model.maxpool = nn.Identity()  # type: ignore
-    model.avgpool = nn.AvgPool2d(4)  # type: ignore
-    model.fc = nn.Linear(2048, num_classes)
-    return model
+@timm_register_model
+def resnet50_cifar10(pretrained=False, **kwargs):
+    return _create_resnet_small("resnet50_cifar10", features_dim=2048, pretrained=pretrained, **kwargs)
 
 
-def get_default_features_nodes() -> List[str]:
-    return []
+@timm_register_model
+def resnet18_cifar100(pretrained=False, **kwargs):
+    return _create_resnet_small("resnet18_cifar100", features_dim=512, pretrained=pretrained, **kwargs)
+
+
+@timm_register_model
+def resnet34_cifar100(pretrained=False, **kwargs):
+    return _create_resnet_small("resnet34_cifar100", features_dim=512, pretrained=pretrained, **kwargs)
+
+
+@timm_register_model
+def resnet50_cifar100(pretrained=False, **kwargs):
+    return _create_resnet_small("resnet50_cifar100", features_dim=2048, pretrained=pretrained, **kwargs)
 
 
 if __name__ == "__main__":
-    model = ResNet18Small()
-    print(model)
-    model = ResNet34Small()
-    print(model)
-    model = ResNet101Small()
-    print(model)
+    model_obj_1 = timm.create_model("resnet18", pretrained=False)
+    model_obj_2 = timm.create_model("resnet18_cifar10", pretrained=False)
+    print(model_obj_1.conv1)
+    print(model_obj_2.conv1)
 
-    model = resnet34_cifar10(weights=False)
+    import torch.fx as fx
+    from torchvision.models.feature_extraction import create_feature_extractor, get_graph_node_names
 
-    x = torch.randn(1, 3, 32, 32)
-    y = model(x)
-    print(y)
-    assert y.shape == (1, 10)
+    model = timm.create_model("resnet18_cifar10", pretrained=False)
+    print(get_graph_node_names(model)[0])
+    graph: fx.Graph = fx.Tracer().trace(model)
+    import timm.data
 
-    model = resnet18_cifar10(weights=False)
-    print("resnet18", model(x))
+    data_config = timm.data.resolve_data_config(model.default_cfg)
+    train_transforms = timm.data.create_transform(**data_config, is_training=True, color_jitter=0)
+    test_transforms = timm.data.create_transform(**data_config)
+    print(train_transforms)
+    print(test_transforms)
