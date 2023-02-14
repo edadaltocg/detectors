abcdb6d7b9c8fcbd9ff82e7431822dbb4e896477
Eduardo Dadalto <edadaltocg@gmail.com>
2023-02-14 13:21:30 +0100

    small changes

--- src/detectors/config.py
+++ src/detectors/config.py
@@ -1,9 +1,8 @@
 import os
 
 
-DATASETS_DIR = os.environ.get("DATASETS_DIR", "data/")
-IMAGENET_ROOT = os.environ.get("IMAGENET_ROOT", DATASETS_DIR)
+DATA_DIR = os.environ.get("DATA_DIR", "data/")
+IMAGENET_ROOT = os.environ.get("IMAGENET_ROOT", DATA_DIR)
 CHECKPOINTS_DIR = os.environ.get("CHECKPOINTS_DIR", "checkpoints/")
 TENSORS_DIR = os.environ.get("TENSORS_DIR", "tensors/")
-
 RESULTS_DIR = os.environ.get("RESULTS_DIR", "results/")
