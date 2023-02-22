345086aed3172910d7c7c6c457837fe1258cebaa
Eduardo Dadalto <edadaltocg@gmail.com>
2023-02-22 17:32:41 +0100

    change point detection module

---
+++ src/detectors/methods/cpd/__init__.py
@@ -0,0 +1,12 @@
+from detectors.methods.cpd.pelt import PeltL2
+
+
+cpd_registry = {
+    "peltl2": PeltL2,
+}
+
+
+def create_cpd(cpd_name, **kwargs):
+    if cpd_name not in cpd_registry:
+        raise ValueError(f"CPD {cpd_name} not found")
+    return cpd_registry[cpd_name](**kwargs)
