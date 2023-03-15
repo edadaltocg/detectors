c2671fc11c9026455d3557cd3cad35a58a3018b3
Eduardo Dadalto <edadaltocg@gmail.com>
2023-03-15 16:08:35 +0100

    accelerate compute accuracy script

--- scripts/compute_accuracy.py
+++ scripts/compute_accuracy.py
@@ -3,6 +3,7 @@ import json
 import logging
 import time
 
+import accelerate
 import numpy as np
 import timm
 import torch
@@ -18,29 +19,40 @@ _logger = logging.getLogger(__name__)
 
 @torch.no_grad()
 def main(args):
-    device = args.device
+    accelerator = accelerate.Accelerator()
+
     model = timm.create_model(args.model, pretrained=True)
-    model.to(device)
-    model.eval()
     data_config = resolve_data_config(model.default_cfg)
     transform = create_transform(**data_config)
     _logger.info(transform)
+
+    model.eval()
+    model = accelerator.prepare(model)
+
     dataset = detectors.create_dataset(args.dataset, split=args.split, transform=transform)
     dataloader = torch.utils.data.DataLoader(
-        dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=True
+        dataset, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=accelerator.num_processes
     )
+    dataloader = accelerator.prepare(dataloader)
 
     inference_time = []
     start_time = time.time()
     acc = 0
-    for x, labels in tqdm(dataloader):
-        x = x.to(device, non_blocking=True)
+    progress_bar = tqdm(dataloader, desc="Inference", disable=not accelerator.is_local_main_process)
+    for x, labels in dataloader:
         t1 = time.time()
         outputs = model(x)
         t2 = time.time()
-        inference_time.append(t2 - t1)
         _, preds = torch.max(outputs, 1)
-        acc += torch.sum(preds.cpu() == labels.data).item()
+
+        preds, labels = accelerator.gather_for_metrics((preds, labels))
+        acc += torch.sum((preds.cpu() == labels.cpu()).float()).item()
+
+        inference_time.append(t2 - t1)
+        progress_bar.update()
+    progress_bar.close()
+    accelerator.wait_for_everyone()
+
     print(f"Total time: {time.time() - start_time:.2f} seconds")
     print(f"Accuracy: {acc / len(dataset)}")
     print(f"Average inference time: {np.mean(inference_time)}")
@@ -55,8 +67,6 @@ if __name__ == "__main__":
     parser.add_argument("--dataset", type=str, default="imagenet1k")
     parser.add_argument("--split", type=str, default="val")
     parser.add_argument("--batch_size", type=int, default=128)
-    parser.add_argument("--num_workers", type=int, default=0)
-    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available() else "cpu")
 
     args = parser.parse_args()
 
