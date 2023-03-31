d82efcc7b4dd5500e2477421c8df6e282a3020e7
Eduardo Dadalto <edadaltocg@gmail.com>
2023-03-31 10:12:20 +0200

    list available resources

---
+++ examples/list_resources.py
@@ -0,0 +1,20 @@
+"""Example on how to list all resources available in `detectors` package."""
+import detectors
+
+if __name__ == "__main__":
+    # list all available models
+    print("Models:")
+    print(detectors.list_models())
+    print(detectors.list_models("*cifar*"))
+
+    # list all available datasets
+    print("Datasets:")
+    print(detectors.list_datasets())
+
+    # list all available detectors
+    print("Detectors:")
+    print(detectors.list_detectors())
+
+    # list all available pipelines
+    print("Pipelines:")
+    print(detectors.list_pipelines())
