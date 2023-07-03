9626764755a55ebff5a2f82cddc72a2e9f7a5c41
Eduardo Dadalto <edadaltocg@gmail.com>
2023-07-03 12:03:42 +0200

    fix typo

--- examples/ood_benchmark.py
+++ examples/ood_benchmark.py
@@ -3,14 +3,12 @@ import json
 import logging
 import os
 
-import accelerate
 import timm
 import timm.data
 import torch
 
 import detectors
 from detectors.config import RESULTS_DIR
-from detectors.utils import str_to_dict
 
 _logger = logging.getLogger(__name__)
 
@@ -29,11 +27,11 @@ def main(args):
         args.pipeline, batch_size=args.batch_size, seed=args.seed, transform=test_transform, limit_fit=args.limit_fit
     )
 
-    if "vit" in args.model and "pooling_op_name" in args.methods_kwargs:
-        args.methods_kwargs[args.method]["pooling_op_name"] = "getitem"
+    if "vit" in args.model and "pooling_op_name" in args.method_kwargs:
+        args.method_kwargs["pooling_op_name"] = "getitem"
 
     # create detector
-    method = detectors.create_detector(args.method, model=model, **args.methods_kwargs)
+    method = detectors.create_detector(args.method, model=model, **args.method_kwargs)
     # run pipeline
     pipeline_results = pipeline.run(method)
     # print results
@@ -45,7 +43,7 @@ def main(args):
             "model": args.model,
             "method": args.method,
             **pipeline_results["results"],
-            "method_kwargs": args.methods_kwargs,
+            "method_kwargs": args.method_kwargs,
         }
         filename = os.path.join(RESULTS_DIR, args.pipeline, "results.csv")
         detectors.utils.append_results_to_csv_file(results, filename)
@@ -58,7 +56,7 @@ def main(args):
             "in_dataset_name": pipeline.in_dataset_name,
             "out_datasets_names": pipeline.out_datasets_names,
             "method": args.method,
-            "method_kwargs": args.methods_kwargs,
+            "method_kwargs": args.method_kwargs,
             "scores": scores.numpy().tolist(),
             "labels": labels.numpy().tolist(),
         }
@@ -69,7 +67,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--method", type=str, default="msp")
-    parser.add_argument("--methods_kwargs", type=str_to_dict, default={}, help='{"temperature":1000, "eps":0.00014}')
+    parser.add_argument("--method_kwargs", type=json.loads, default={}, help='{"temperature":1000, "eps":0.00014}')
     parser.add_argument("--pipeline", type=str, default="ood_benchmark_cifar10")
     parser.add_argument("--model", type=str, default="resnet18_cifar10")
     parser.add_argument("--batch_size", type=int, default=256)
