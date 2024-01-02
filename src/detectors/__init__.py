00c0dc1c2a949a618994be51742d6ac824805bcf
Eduardo Dadalto <edadaltocg@gmail.com>
2024-01-01 22:38:37 -0300

    minor fix

--- src/detectors/__init__.py
+++ src/detectors/__init__.py
@@ -7,7 +7,7 @@ from .aggregations import create_aggregation, list_aggregations, register_aggreg
 from .data import create_dataset, get_dataset_cls, list_datasets, register_dataset
 from .eval import get_ood_results
 from .methods import create_detector, create_hyperparameters, list_detectors, register_detector
-from .methods.utils import create_reduction
 from .methods.templates import Detector
+from .methods.utils import create_reduction
 from .models import create_model, create_transform, list_models, timm_register_model
 from .pipelines import create_pipeline, list_pipelines, register_pipeline
